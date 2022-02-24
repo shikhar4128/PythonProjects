5b86f803631ba8a2df9343397910569940710d1c
Shikhar Srivastav <shikhar4128@gmail.com>
2022-02-24 17:17:33 -0500

    TicTac

--- TicTacToe.py
+++ TicTacToe.py
@@ -4,17 +4,11 @@ import random
 #1. A function that can print out a board.
 def display_board(board):
     clear_output()
-    print('   |   |')
     print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
-    print('   |   |')
     print('-----------')
-    print('   |   |')
     print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
-    print('   |   |')
     print('-----------')
-    print('   |   |')
     print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
-    print('   |   |')
 
 #2. A function that can take in a player input and assign their marker as 'X' or 'O'.
 def player_input():
